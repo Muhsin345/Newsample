aafaabc4a1bfc3c8ddec7754bd35348b7d5724e2
Muhsin345 <mzmuhzin123@gmail.com>
2021-09-01 12:54:43 +0530

    tmp

--- App2/urls.py
+++ App2/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     path('Sample2',views.fnName2),
     path('Name2',views.fnName3),
     path('Home',views.fnHome),
-    path('Data1/',views.fnData),
-   # path('Login',views.fnLogin)
+    path('Data1/',views.fnData)
+       # path('Login',views.fnLogin)
 ]
