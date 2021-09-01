0962422a13e353d8e62cc940cefab80cba8e610e
Muhsin345 <mzmuhzin123@gmail.com>
2021-09-01 12:48:48 +0530

    chng

--- App2/urls.py
+++ App2/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     path('Sample2',views.fnName2),
     path('Name2',views.fnName3),
     path('Home',views.fnHome),
-    path('Data/',views.fnData),
+    path('Data1/',views.fnData),
    # path('Login',views.fnLogin)
 ]
